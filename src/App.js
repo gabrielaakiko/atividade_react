a0c9b918bae32549c93f6b4742cdd2826b555302
Seu nome <fioregabrila16@gmail.com>
2021-11-24 12:40:36 -0300

    Primeira versão do projeto

---
+++ src/App.js
@@ -0,0 +1,47 @@
+
+import{ BrowserRouter as Router, Switch, Route, Link }from 'react-router-dom'
+
+import Home from './components/pages/Home'
+import Sobre from './components/pages/Sobre'
+import Contato from './components/pages/Contato'
+import NovoProjeto from './components/pages/NovoProjeto'
+
+import Container from './components/layout/Container'
+
+function App() {
+
+  return (
+    <Router>
+      <div>
+        <Link to="/">Home/</Link>
+        <Link to="/sobre">Sobre/</Link>
+        <Link to="/contato">Contato/</Link>
+        <Link to="/novoprojeto">NovoProjeto/</Link>
+      </div>
+      <ul>
+        <li>Home</li>
+        <li>Contato</li>
+      </ul>
+      <Switch>
+        <Container>
+          <Route exact path="/">
+            <Home/>
+          </Route>
+          <Route exact path="/sobre">
+           <Sobre />
+          </Route>
+          <Route exact path="/contato">
+           <Contato />
+          </Route>
+          <Route exact path="/novoprojeto">
+           <NovoProjeto />
+          </Route>
+        </Container>
+      </Switch>
+      <p>Rodapé</p>
+    </Router>
+    
+  );
+}
+
+export default App;
